16450042a6b7b4d747613b64a0ac8705178aa4ce
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-06-05 16:27:49 -0400

    Created Vector2D.swift

---
+++ Sources/GeometryEngine/Vector2D.swift
@@ -0,0 +1,16 @@
+// Vector2D.swift
+// Created by Jérémy Goyette
+// For GeometryEngine
+// June 5th, 2020
+//
+
+import Foundation
+
+/// A vector in 2D space. It can be used for describing position and disrection or for various mathematical calculations.
+/// You can create a new instance by using an arry litteral:
+///
+///     let v1: Vector2D = [0.0, 1.0]
+///
+struct Vector2D {
+
+}
