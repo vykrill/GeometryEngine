a6f6281550add7fb57d79f7d13515ffec62ea40e
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-06-05 16:53:56 -0400

    Added length and angle properties

--- Sources/GeometryEngine/Vector2D.swift
+++ Sources/GeometryEngine/Vector2D.swift
@@ -14,6 +14,23 @@ import Foundation
 struct Vector2D: Equatable, ExpressibleByArrayLiteral {
     private var storage = SIMD2<Double>()
 
+    /// The length of the vector.
+    var length: Double { sqrt(pow(self.x, 2) + pow(self.y, 2)) }
+
+    /// The angle of the vector in radians.
+    var angle: Double { 
+        var adjustment = 0.0
+        switch (self.x, self.y) {
+        case (let dX, _) where dX < 0:
+            adjustment = π
+        case (let dX, let dY) where dX > 0 && dY < 0:
+            adjustment = 2 * π
+        default:
+            break
+        }
+        return atan(self.y / self.x) + adjustment
+    }
+
     /// Creates a new null vector.
     init() {self.storage = [0.0, 0.0] }
 
