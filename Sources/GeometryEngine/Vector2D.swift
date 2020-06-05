8c1e91892ad0b9a79dd5b0ec83e78f02356de783
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-06-05 16:35:42 -0400

    Added storage to Vector2D

--- Sources/GeometryEngine/Vector2D.swift
+++ Sources/GeometryEngine/Vector2D.swift
@@ -11,6 +11,10 @@ import Foundation
 ///
 ///     let v1: Vector2D = [0.0, 1.0]
 ///
+@dynamicMemberLookup
 struct Vector2D {
+    private var storage = SIMD2<Double>()
 
+    // - MARK: @dynamicMemberLookup
+    subscript(dynamicMember keyPath: KeyPath<SIMD2<Double>, Double>) -> Double { storage[keyPath: keyPath] }
 }
