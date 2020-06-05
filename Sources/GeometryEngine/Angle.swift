a1b5f8c265a081648afd19f187210724b1669e67
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-06-05 12:10:39 -0400

    Created Angle.swift

---
+++ Sources/GeometryEngine/Angle.swift
@@ -0,0 +1,29 @@
+/// Angle.swift
+///
+/// A property wrapper for angles
+
+@propertyWrapper
+struct Angle {
+
+    private var value: Double
+
+    var wrappedValue: Double {
+        get { value }
+        set { 
+            value = newValue
+            bounds(&value)
+         }
+    }
+
+    /// Bounds the specified angle to a value between `0` and `2π`.
+    /// - parameters:
+    ///     - angle: The angle to bounds.
+    private func bounds(_ angle: inout Double) {
+        while angle < 0 {
+            angle += 2 * π
+        }
+        while angle > 2 * π {
+            angle -= 2 * π
+        }
+    }
+}
