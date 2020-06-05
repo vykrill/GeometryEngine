ba49ecf943c60f38735deac37ccc326895e879e8
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-06-05 16:42:51 -0400

    Added initialisers to Vector2D

--- Sources/GeometryEngine/Vector2D.swift
+++ Sources/GeometryEngine/Vector2D.swift
@@ -10,11 +10,20 @@ import Foundation
 /// You can create a new instance by using an arry litteral:
 ///
 ///     let v1: Vector2D = [0.0, 1.0]
-///
 @dynamicMemberLookup
-struct Vector2D {
+struct Vector2D: Equatable, ExpressibleByArrayLiteral {
     private var storage = SIMD2<Double>()
+
+    /// Creates a new null vector.
+    init() {self.storage = [0.0, 0.0] }
 
     // - MARK: @dynamicMemberLookup
     subscript(dynamicMember keyPath: KeyPath<SIMD2<Double>, Double>) -> Double { storage[keyPath: keyPath] }
+
+    // - MARK: ExpressibleByArrayLitteral
+    typealias ArrayLiteralElement = Double
+    
+    init(arrayLiteral elements: ArrayLiteralElement...) {
+        self.storage = SIMD2<Double>(elements)
+    }
 }
