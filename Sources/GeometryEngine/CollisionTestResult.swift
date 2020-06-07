f57de582395ba25449d55c51331b9d4d20c07443
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-06-07 17:40:56 -0400

    Created CollisionTestResult enum

---
+++ Sources/GeometryEngine/CollisionTestResult.swift
@@ -0,0 +1,15 @@
+// CollisionTestResult.swift
+// Created by Jérémy Goyette
+// For GeometryEngine
+// June 7th, 2020
+//
+
+enum CollisionTestResult {
+
+    /// There is no collision.
+    case none
+    /// There is a contact point.
+    case point(position: Vector2D)
+    /// Two lines overlap each other.
+    case segment(section: Line)
+}
