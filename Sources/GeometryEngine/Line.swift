e7f70739b06df21e444e9a13bf013f5d8d15cba7
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-06-08 17:17:20 -0400

    Worked on the documentation

--- Sources/GeometryEngine/Line.swift
+++ Sources/GeometryEngine/Line.swift
@@ -8,7 +8,8 @@ import Foundation
 
 /// A line in 2D space.
 ///
-/// A line is represented by two `Vector2D` instances. It can then be used for composing polygons. You create a new `Line` by giving its twos points.
+/// A line is represented by two `Vector2D` instances. It can then be used for composing 
+///  polygons. You create a new `Line` by giving its twos points.
 ///
 ///     // Creates a line from (0, 0) to (1, 0).
 ///     let line = Line([0, 0], [1, 0])
@@ -58,6 +59,13 @@ struct Line: Equatable {
         }
     }
 
+    /// Checks for intersection between two lines.
+    /// - parameters: 
+    ///     * line1: The first line.
+    ///     * line2: The other line.
+    ///
+    /// - returns: The appropriate `CollisionTestResult` depending of the intersection.
+    ///             Check `CollisionTestResult`'s documentation for more detail. 
     static func getIntersectionBetween(_ line1: Line, and line2: Line) -> CollisionTestResult {
         if let point = Line.getIntersectionPointBetween(line1, and: line2) {
             // The two lines are not parallel.
@@ -72,31 +80,45 @@ struct Line: Equatable {
         }
     }
 
+    /// Checks for the intersection point between two lines.
+    /// - parameters:
+    ///     * line1: The first line.
+    ///     * line2: The other line.
+    /// - note: The returned point might not be part of either line as they will be 
+    ///          considered like infinite lines.
+    /// - returns: `nil` if the two lines are parallel, otherwise the intersection point.
     static private func getIntersectionPointBetween(_ line1: Line, and line2: Line) -> Vector2D? {
         guard !line1.vector.isParallel(to: line2.vector) else { return nil }
 
-        // Angle between the two lines
+        // Angle between the two lines.
         let angle1 = Angle.getDifferenceBetween(line1.angle, and: line2.angle)
         
         let vector1 = line2.p2 - line1.p1
 
-        // Angle between line1 and vector1
+        // Angle between line1 and vector1.
         var angle2 = Angle.getDifferenceBetween(vector1.angle, and: line1.angle)
         if angle2 > π { angle2 -= π }
 
-        // Length to remove from line2
+        // Length to remove from line2.
         let length = (sin(angle2) * vector1.length) / sin(angle1)
         let vector2 = Vector2D(length: length, angle: line2.angle + π)
 
         return line2.p2 + vector2
     }
 
+    /// Checks for the overlap between two colinear lines.
+    /// - parameters:
+    ///     * line1: The first line.
+    ///     * line2: The second line.
+    /// - returns: `nil` if the two lines are not colinear. If they are, it returns the line 
+    ///             composing the overlap. The only exception is when the lenght of the line
+    ///             is null; it then returns the point.
     static private func getOverlapBetween(_ line1: Line, and line2: Line) -> CollisionTestResult {
-        // We first check if the two lines are colinear
+        // We first check if the two lines are colinear.
         let testVector: Vector2D = [line2.p1.x - line1.p1.x, line2.p1.y - line1.p1.y]
         guard testVector.isParallel(to: line1.vector) else { return .none }
 
-        // We then ensure there is an overlap
+        // We then ensure there is an overlap.
         let vector: Vector2D = [line1.minX - line2.minX, line1.minY - line2.minY]
         guard !(vector.length > line1.length && vector.length > line2.length) else {
             return .none
@@ -106,8 +128,10 @@ struct Line: Equatable {
         // It is from the highest minX/minY to the lowest maxX/maxY.
         let result = Line(
             [max(line1.minX, line2.minX), max(line1.minY, line2.minY)],
-            [min(line1.maxX, line2.maxY), min(line1.maxY, line2.maxY)]
-        )
+            [min(line1.maxX, line2.maxY), min(line1.maxY, line2.maxY)])
+
+        // For clarity, we return a point if the length of the result line is null.
+        if result.p1 == result.p2 { return .point(position: result.p1) }
 
         return .segment(section: result)
     }
