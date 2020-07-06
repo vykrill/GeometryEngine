3734c2ae633638a2ca322457d41740cb1776e869
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-07-05 20:09:08 -0400

    created the GeometryPrimitive protocol.

---
+++ Sources/GeometryEngine/Primitives/GeometryPrimitiveProtocol.swift
@@ -0,0 +1,11 @@
+// GeometryPrimitiveProtocol.swift
+// Created by Jérémy Goyette
+// For GeometryEngine
+// June 7th, 2020
+//
+
+/// A component of a shape.
+protocol GeometryPrimitive {
+    func isPointInward(_: Vector2D, clockwise: Bool)
+    
+}
