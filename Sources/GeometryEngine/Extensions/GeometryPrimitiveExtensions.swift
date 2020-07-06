5b633ed3632797ca722741ad38960cf4a9e11bf3
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-07-05 20:17:49 -0400

    Added lines collision support to GeometryPrimitive

---
+++ Sources/GeometryEngine/Extensions/GeometryPrimitiveExtensions.swift
@@ -0,0 +1,11 @@
+// GeometryPrimitiveExtensions.swift
+// Created by Jérémy Goyette
+// For GeometryEngine
+// June 7th, 2020
+//
+
+extension GeometryPrimitive {
+    // MARK: - Intersections with lines.
+
+    func intersection(with: Line) -> CollisionTestResult { .none }
+}
