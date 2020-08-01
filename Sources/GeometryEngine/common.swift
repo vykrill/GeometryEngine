9827f0f13ff5e0f7878f90c3178bbae661e03889
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-08-01 12:25:07 -0400

    Encapsulated intersection() in CommonFunctions

--- Sources/GeometryEngine/common.swift
+++ Sources/GeometryEngine/common.swift
@@ -10,6 +10,10 @@ import Foundation
 /// The constant pi, in a shorter form.
 let π = Double.pi
 
+/// A container for various functions used through the Geometry Engine.
+enum CommonFunctions {
+
+
 /// Returns the intersection between two `GeometryPrimitive`.
 ///
 /// Currently, this function support intersection between these primitive pairs:
@@ -18,11 +22,12 @@ let π = Double.pi
 /// - returns: `.none` if intersection detection between the two primitives is not supported, 
 ///             otherwise the intersection found. Note that supported primitives can also 
 ///             return `.none` if no collision is found.
-func intersection(between primitive1: GeometryPrimitive, and primitive2: GeometryPrimitive) -> CollisionTestResult {
+static func intersection(between primitive1: GeometryPrimitive, and primitive2: GeometryPrimitive) -> CollisionTestResult {
     switch (primitive1, primitive2) {
     case (let line1 as Line, let line2 as Line):
         return line1.intersection(with: line2)
     default:
         return .none
     }
+}
 }
