7e4b90b45d2a88409ce0a7779731177ed0f205b7
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-08-10 18:26:32 -0400

    added documentation.

--- Sources/GeometryEngine/Shapes/GeometryProtocol.swift
+++ Sources/GeometryEngine/Shapes/GeometryProtocol.swift
@@ -8,6 +8,7 @@ protocol Geometry {
     /// The basic geometry which compose the shape.
     var primitives: [GeometryPrimitive] { get }
     
+    /// The points composing the shape.
     var vertices: [Vector2D] { get }
 
     /// If the primitives are created in a clockwise order or not. Usefull for collision detection.
@@ -16,11 +17,30 @@ protocol Geometry {
     /// Returns whether or not this geometry contains the specified point.
     func contains(_ point: Vector2D) -> Bool
     
+    /// Returns the points which are contained in the shape.
+    ///
+    /// It basically runs the `contains(_:)` method on each point of the array.
+    ///
+    /// - parameter points: The array of point to test.
+    /// - returns: An array of `.point`, which are the points contained in the
+    ///             shape.
     func contains(_ points: [Vector2D]) -> [CollisionTestResult]
     
-    /// Returns whether or not there is a collision between two shapes.
+    /// Returns whether or not there is an intersection between two shapes.
+    ///
+    /// - note: This method only checks for intersections between primitives.
+    ///          For a complete collision detection, see the
+    ///          `collision(with:)` method.
+    /// - parameter geometry: The geometry with which we check for
+    ///                         intersection.
+    /// - returns: An array of `CollisionTestResult` containing every
+    ///             intersection detected.
     func intersection(with geometry: Geometry) -> [CollisionTestResult]
     
+    /// Complete collision detection with another shape.
+    /// - parameter geometry: The other geometry.
+    /// - returns: An array of `CollisionTestResult` containing every
+    ///             collision detected.
     func collision(with geometry: Geometry) -> [CollisionTestResult]
 }
 
